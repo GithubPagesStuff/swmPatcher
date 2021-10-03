3c07f19ccdcc8027b31dcff7e3c8322d42657ae8
comrade <aiden@aidens-macbook-pro.local>
2021-10-03 17:22:45 -0400

    Initial Commit

---
+++ swmPatcher/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  swmPatcher
+//
+//  Created by Aiden on 10/3/21.
+//  Copyright © 2021 bobles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
